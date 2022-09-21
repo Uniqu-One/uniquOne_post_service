178a5101ef4dfdb688fb91f5d4b19a668c68c4ca
Seong Beom <bum090877@gmail.com>
2022-09-21 16:19:12 +0900

    first commit

---
+++ src/test/java/com/sparos/uniquone/msapostservice/MsaPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparos.uniquone.msapostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
