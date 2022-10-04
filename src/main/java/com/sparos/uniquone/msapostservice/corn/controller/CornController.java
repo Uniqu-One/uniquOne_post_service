7e049f05b1e2e850502c8a9b771c57f11cc35829
Seong Beom <bum090877@gmail.com>
2022-10-04 22:51:07 +0900

    S3 api

--- src/main/java/com/sparos/uniquone/msapostservice/corn/controller/CornController.java
+++ src/main/java/com/sparos/uniquone/msapostservice/corn/controller/CornController.java
@@ -4,10 +4,10 @@ import com.sparos.uniquone.msapostservice.corn.dto.CornCreateDto;
 import com.sparos.uniquone.msapostservice.corn.service.ICornService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CornController {
     private final ICornService iCornService;
     @PostMapping("")
-    public ResponseEntity<CornCreateDto> cornInput(@RequestBody CornCreateDto cornCreateDto){
-        iCornService.AddCorn(cornCreateDto);
+    public ResponseEntity<CornCreateDto> cornInput(@RequestPart CornCreateDto cornCreateDto,@RequestPart(value = "imgfile", required = false) MultipartFile multipartFile) throws IOException {
+        iCornService.AddCorn(cornCreateDto, multipartFile);
         return ResponseEntity.ok(cornCreateDto);
     }
 
