70121e2dd100c6f020a5d812193ccc66191efff8
yeon <sy423513@gmail.com>
2022-10-06 17:26:12 +0900

    ➕ 채팅 - 게시물 정보 요청(초기)

--- src/main/java/com/sparos/uniquone/msapostservice/post/repository/IPostRepository.java
+++ src/main/java/com/sparos/uniquone/msapostservice/post/repository/IPostRepository.java
@@ -1,7 +1,10 @@
 package com.sparos.uniquone.msapostservice.post.repository;
 
+import com.sparos.uniquone.msapostservice.corn.domain.Corn;
 import com.sparos.uniquone.msapostservice.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IPostRepository extends JpaRepository<Post, Long> {
+
+    Post findByCornId(Long cornId);
 }
