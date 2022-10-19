7a029bbcad06bd00f3cf314cf481990eb5c780d5
seongbeom7 <bum090877@gmail.com>
2022-10-19 17:57:32 +0900

    postservice dev

--- src/main/java/com/sparos/uniquone/msapostservice/post/service/IPostService.java
+++ src/main/java/com/sparos/uniquone/msapostservice/post/service/IPostService.java
@@ -3,6 +3,7 @@ package com.sparos.uniquone.msapostservice.post.service;
 import com.sparos.uniquone.msapostservice.post.domain.Post;
 import com.sparos.uniquone.msapostservice.post.dto.PostChatResponseDto;
 import com.sparos.uniquone.msapostservice.post.dto.PostInputDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,10 +20,11 @@ public interface IPostService {
 
     Object getOtherPostAllList(Long userId, Long cornId);
 
-    Object getOtherPostProductList(Long userId, Long cornId);
+    Object getOtherPostProductList(Long userId, Long cornId, Pageable pageable);
 
     Object getOtherPostStyleList(Long userId, Long cornId);
 
     Object getModPostInfo(Long userId, Long postId);
+
 
 }
