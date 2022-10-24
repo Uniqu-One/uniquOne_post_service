a3a17b246e4d9f9097e114ad41a69408a2253106
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2022-10-24 12:10:02 +0900

    create search ing.... - DOKYUN

--- src/main/java/com/sparos/uniquone/msapostservice/post/dto/response/search/PostSearchTitleResponseDto.java
+++ src/main/java/com/sparos/uniquone/msapostservice/post/dto/response/search/PostSearchTitleResponseDto.java
@@ -3,10 +3,11 @@ package com.sparos.uniquone.msapostservice.post.dto.response.search;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Slf4j
 public class PostSearchTitleResponseDto {
     //postId,postImg Url, cool
     private Long postId;
@@ -16,5 +17,13 @@ public class PostSearchTitleResponseDto {
     public PostSearchTitleResponseDto(Long postId, String imgUrl) {
         this.postId = postId;
         this.imgUrl = imgUrl;
+    }
+
+    public PostSearchTitleResponseDto(Long postId, String imgUrl, boolean isCool){
+        this.postId = postId;
+        this.imgUrl = imgUrl;
+        //TODO : 이부분은 QueryDsl 부분에서 처리할수 있는 방법을 찾아보자. 아니면 쿼리 두개 날리든가.
+//        log.info("여길 들어오나?");
+        this.isCool = !isCool;
     }
 }
--- src/main/java/com/sparos/uniquone/msapostservice/post/repository/search/SearchPostRepositoryImpl.java
+++ src/main/java/com/sparos/uniquone/msapostservice/post/repository/search/SearchPostRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparos.uniquone.msapostservice.post.domain.Post;
+import com.sparos.uniquone.msapostservice.post.domain.QPostTag;
 import com.sparos.uniquone.msapostservice.post.dto.response.search.PostSearchTitleResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -14,21 +15,28 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.sparos.uniquone.msapostservice.cool.domain.QCool.cool;
 import static com.sparos.uniquone.msapostservice.post.domain.QPost.post;
 import static com.sparos.uniquone.msapostservice.post.domain.QPostImg.postImg;
+import static com.sparos.uniquone.msapostservice.post.domain.QPostTag.postTag;
 
 @Slf4j
 @Repository
 public class SearchPostRepositoryImpl extends QuerydslRepositorySupport implements SearchPostRepository {
 
     private final JPAQueryFactory queryFactory;
+
     public SearchPostRepositoryImpl(JPAQueryFactory queryFactory) {
         super(Post.class);
         this.queryFactory = queryFactory;
     }
 
+    //**헷갈리면 봐라 LEFT JOIN -> [ A left join B on (a.id = b.id) ]
+    //A(왼쪽) 테이블과 B(오른쪽) 테이블을 조인을 걸건데,
+    // B테이블에 A테이블과 맵핑되는 값이 있건 없건 A의 값은 반드시 모두 나오게 됩니다.
+
     @Override
-    public Page<Object[]> searchPostPage(String keyword, Pageable pageable) {
+    public Page<PostSearchTitleResponseDto> searchPostPageOfNonUser(String keyword, Pageable pageable) {
 
 //        QPostImg qPostImg = QPostImg.postImg;
         //해당 키워드가 포함된 포스트 + 포스트 이미지 1번.
@@ -37,21 +45,109 @@ public class SearchPostRepositoryImpl extends QuerydslRepositorySupport implemen
                         , post.id.as("postId")
                         , postImg.url.as("imgUrl")))
                 .from(post)
-                .leftJoin(postImg.post, post).on(postImg.idx.eq(1))
-                .where(searchPostTitle(keyword))
+                .leftJoin(postImg).on(post.eq(postImg.post))
+                .where(searchPostImgIdxEq()
+                        , searchPostTitleContain(keyword))
                 .fetch();
 
+        for (PostSearchTitleResponseDto dto : result) {
+            System.out.println("dto = " + dto);
+        }
+        return null;
+    }
+
+    @Override
+    public Page<PostSearchTitleResponseDto> searchPostPageOfUser(String keyword, Long userPkId, Pageable pageable) {
+        //해당 키워드가 포환된 포스트 + 유저가해당 포스트 좋아요 유무 + 포스트 이미지 1번.
+        List<PostSearchTitleResponseDto> result = queryFactory
+                .select(Projections.constructor(PostSearchTitleResponseDto.class
+                        , post.id.as("postId")
+                        , postImg.url.as("imgUrl")
+                        , cool.id.isNull().as("isCool")
+                        )
+                )
+                .from(post)
+                .leftJoin(postImg).on(post.eq(postImg.post))
+                .leftJoin(cool).on(post.eq(cool.post))
+//                .leftJoin(cool).on(cool.userId.eq(userPkId))
+                .where(searchPostImgIdxEq()
+                        , searchPostTitleContain(keyword)
+                        , searchCoolUserPkIdEq(userPkId)
+                )
+                .fetch();
+
+        for (PostSearchTitleResponseDto dto : result) {
+            System.out.println("dto = " + dto);
+        }
+        return null;
+    }
+
+    @Override
+    public Page<PostSearchTitleResponseDto> searchHashTagPageOfNonUser(String keyword, Pageable pageable) {
+
+        //포스트 의 입력된 키워드와 같은 헤쉬태그 이름 + 포스트 이미지 1번.
+
+        List<PostSearchTitleResponseDto> result = queryFactory
+                .select(Projections.fields(PostSearchTitleResponseDto.class
+                        , post.id.as("postId")
+                        , postImg.url.as("imgUrl")
+                        )
+                )
+                .from(post)
+                .leftJoin(postImg).on(post.eq(postImg.post))
+                .leftJoin(postTag).on(post.eq(postTag.post))
+                .where(searchHasTagEq(keyword)
+                        , searchPostImgIdxEq())
+                .fetch();
 
         for (PostSearchTitleResponseDto dto : result) {
             System.out.println("dto = " + dto);
         }
 
         return null;
-
     }
 
-    public BooleanExpression searchPostTitle(String keyword){
+    @Override
+    public Page<PostSearchTitleResponseDto> searchHashTagPageOfUser(String keyword,Long userPkId ,Pageable pageable) {
+
+        //포스트 의 입력된 키워드와 같은 헤쉬태그 이름 + 포스트 이미지 1번 + 유저의 포스트 좋아요 유무.
+        List<PostSearchTitleResponseDto> result = queryFactory
+                .select(Projections.constructor(PostSearchTitleResponseDto.class
+                                , post.id.as("postId")
+                                , postImg.url.as("imgUrl")
+                                , cool.id.isNull().as("isCool")
+                        )
+                )
+                .from(post)
+                .leftJoin(postImg).on(post.eq(postImg.post))
+                .leftJoin(postTag).on(post.eq(postTag.post))
+                .leftJoin(cool).on(post.eq(cool.post))
+                .where(searchHasTagEq(keyword)
+                        , searchPostImgIdxEq()
+                        , searchCoolUserPkIdEq(userPkId)
+                )
+                .fetch();
+
+        for (PostSearchTitleResponseDto dto : result) {
+            System.out.println("dto = " + dto);
+        }
+        return null;
+    }
+
+
+    public BooleanExpression searchPostTitleContain(String keyword) {
         return keyword != null ? post.title.contains(keyword) : null;
+    }
+    public BooleanExpression searchCoolUserPkIdEq(Long userPkId){
+        return userPkId != null ? cool.userId.eq(userPkId) : null;
+    }
+
+    public BooleanExpression searchPostImgIdxEq() {
+        return postImg.idx.eq(1);
+    }
+
+    public BooleanExpression searchHasTagEq(String keyword) {
+        return keyword != null ? postTag.dsc.eq(keyword) : null;
     }
 
 
