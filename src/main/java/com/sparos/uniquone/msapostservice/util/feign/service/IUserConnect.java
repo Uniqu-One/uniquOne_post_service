a0700694e08658b38c40d7b2b9abad59f62ea450
yeon <sy423513@gmail.com>
2022-10-27 10:52:44 +0900

    feign

--- src/main/java/com/sparos/uniquone/msapostservice/util/feign/service/IUserConnect.java
+++ src/main/java/com/sparos/uniquone/msapostservice/util/feign/service/IUserConnect.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "uniquone-user-service")
+@FeignClient(name = "uniquone-user-service", url = "http://gateway-server:8000")
 public interface IUserConnect {
 
-    @GetMapping("/users/get/nickName/{userId}")
+    @GetMapping("/get/nickName/{userId}")
     String getUserNickName(@PathVariable("userId") Long userId);
 
 }
