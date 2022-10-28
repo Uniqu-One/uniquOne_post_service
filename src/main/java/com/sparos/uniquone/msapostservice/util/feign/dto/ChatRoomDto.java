36ec97cf16661b7e2526a0380987d6b5d62a7f81
yeon <sy423513@gmail.com>
2022-10-28 17:27:34 +0900

    offerã api 수정

--- src/main/java/com/sparos/uniquone/msapostservice/util/feign/dto/ChatRoomDto.java
+++ src/main/java/com/sparos/uniquone/msapostservice/util/feign/dto/ChatRoomDto.java
@@ -5,21 +5,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatRoomDto {
 
-    // 나중에 없애기
     private Long receiverId;
 
     private Long postId;
 
     private ChatRoomType chatType;
-
-    private LocalDateTime regDate;
 
 }
