f52d1276b36b0ca012ddff6f6115d3b93a323c97
seongbeom7 <bum090877@gmail.com>
2022-11-07 11:36:33 +0900

    버그 수정

--- src/main/java/com/sparos/uniquone/msapostservice/post/dto/PostRecommendResponseDto.java
+++ src/main/java/com/sparos/uniquone/msapostservice/post/dto/PostRecommendResponseDto.java
@@ -1,11 +1,15 @@
 package com.sparos.uniquone.msapostservice.post.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostRecommendResponseDto {
     private Long postId;
     private String postImgUrl;
@@ -26,7 +30,16 @@ public class PostRecommendResponseDto {
         this.cornImgUrl = cornImgUrl;
         this.userNickName = userNickName;
     }
-    public PostRecommendResponseDto(Long postId,String postImgUrl ,String cornImgUrl, String userNickName, boolean isCool, boolean isFollow, Integer uniStar) {
+
+    public PostRecommendResponseDto(Long postId, String postImgUrl, String cornImgUrl, String userNickName, Long cornId) {
+        this.postId = postId;
+        this.postImgUrl = postImgUrl;
+        this.cornImgUrl = cornImgUrl;
+        this.userNickName = userNickName;
+        this.cornId = cornId;
+    }
+
+    public PostRecommendResponseDto(Long postId, String postImgUrl , String cornImgUrl, String userNickName, boolean isCool, boolean isFollow, Integer uniStar) {
         this.postId = postId;
         this.postImgUrl = postImgUrl;
         this.cornImgUrl = cornImgUrl;
